328e3ce284ee39698a98e7f4089ef23f8733e850
sinc <sinc.i@outlook.com>
2024-04-26 17:11:12 +0800

    init: nuxt pwa

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtPwaManifest></NuxtPwaManifest>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+/* // If you want to use it in setup, import from the nuxtApp.
+const {$pwa} = useNuxtApp();
+
+const toast = useToast();
+
+onMounted(() => {
+  if ($pwa.offlineReady) toast.success("App ready to work offline");
+}); */
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt Vite PWA</h1>
+    <ClientOnly> PWA Installed: {{ $pwa?.isPWAInstalled }} </ClientOnly>
+    <NuxtLink to="/about"> About </NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
